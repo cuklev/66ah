499becdb603205d3a4def597c6b664582c99a2b6
Kristiyan Tsaklev <cuki@cukii.tk>
2016-10-02 18:57:52 +0300

    Add multithreaded solution

---
+++ src/dp2_multithreaded/board.cpp
@@ -0,0 +1,13 @@
+#include "board.h"
+
+#include<iostream>
+
+int main() {
+	const int ROWS = 12;
+	const int COLS = 12;
+
+	Board<COLS> board;
+
+	for(int i = 1; i <= ROWS; ++i)
+		std::cout << COLS << 'x' << i << " -> " << board.getSolution(i) << '\n';
+}
